6e8ed391987a61adf159af87328628cdfe01953c
Adrien Perrin <adrien.perrin@nersc.no>
2020-05-14 12:26:57 +0000

    runtests.py: add ability to specify the test case to run

--- runtests.py
+++ runtests.py
@@ -9,7 +9,8 @@ from django.test.utils import get_runner
 if __name__ == "__main__":
     os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.settings'
     django.setup()
+    test_case = f".{sys.argv[1]}" if len(sys.argv) >= 2 else ''
     TestRunner = get_runner(settings)
-    test_runner = TestRunner(verbosity=1, interactive=True)
-    failures = test_runner.run_tests(["geospaas_rest_api"])
+    test_runner = TestRunner(verbosity=1, interactive=False)
+    failures = test_runner.run_tests(["geospaas_rest_api.tests" + test_case])
     sys.exit(bool(failures))
