2648fd17b5e1ddcb7d8997af195a9f5802049aa4
Adrien Perrin <adrien.perrin@nersc.no>
2021-01-13 15:39:51 +0000

    add tests for the new filters

--- geospaas_rest_api/tests/test_read_only_api.py
+++ geospaas_rest_api/tests/test_read_only_api.py
@@ -118,3 +118,176 @@ class BasicAPITests(django.test.TestCase):
             'long_name': 'Aalto University',
             'data_center_url': 'dummy_included_in_test_mode'
         })
+
+
+class DatasetFilteringTests(django.test.TestCase):
+    """Tests dataset filtering based on diverse parameters"""
+    fixtures = ["read_only_tests_data"]
+
+    DATASET_DICT_1 = {
+        'id': 1,
+        'entry_id': 'NERSC_test_dataset_titusen',
+        'entry_title': 'Test dataset',
+        'summary': 'This is a quite short summary about the test dataset.',
+        'time_coverage_start': '2010-01-01T00:00:00Z',
+        'time_coverage_end': '2010-01-02T00:00:00Z',
+        'access_constraints': None,
+        'ISO_topic_category': 1,
+        'data_center': 1,
+        'source': 1,
+        'geographic_location': 1,
+        'gcmd_location': 1,
+        'parameters': []
+    }
+
+    DATASET_DICT_2 = {
+        'id': 2,
+        'entry_id': 'NERSC_test_dataset_tjuetusen',
+        'entry_title': 'Test child dataset',
+        'summary': 'This is a quite short summary about the test dataset.',
+        'time_coverage_start': '2010-01-02T00:00:00Z',
+        'time_coverage_end': '2010-01-03T00:00:00Z',
+        'access_constraints': None,
+        'ISO_topic_category': 1,
+        'data_center': 2,
+        'source': 2,
+        'geographic_location': 2,
+        'gcmd_location': 1,
+        'parameters': []
+    }
+
+    def test_time_filtering(self):
+        """Test filtering with a date that should be in the dataset time coverage"""
+        date = '2010-01-02 01:00:00Z'
+        response = self.client.get(
+            f'/api/datasets/?time_coverage_start__lte={date}&time_coverage_end__gte={date}')
+        self.assertJSONEqual(response.content, {
+            'next': None, 'previous': None, 'results': [self.DATASET_DICT_2]
+        })
+
+    def test_time_range_filtering(self):
+        """Test filtering with a time range which should intersect with the dataset time coverage"""
+        time_range_start = '2010-01-01T01:00:00Z'
+        time_range_end = '2010-01-02T01:00:00Z'
+        response = self.client.get(
+            '/api/datasets/'
+            f'?time_coverage_start__lte={time_range_end}'
+            f'&time_coverage_end__gte={time_range_start}'
+        )
+        self.assertJSONEqual(response.content, {
+            'next': None, 'previous': None, 'results':[self.DATASET_DICT_1, self.DATASET_DICT_2]
+        })
+
+    def test_time_filtering_error_400_on_wrong_date_format(self):
+        """
+        An error 400 should be returned if the format of the date provided to the filter is invalid
+        """
+        response = self.client.get('/api/datasets/?time_coverage_start__lte=2010-01-02T01:00:Z')
+        self.assertEqual(response.status_code, 400)
+        self.assertJSONEqual(
+            response.content,
+            {"time_coverage_start__lte": ["Enter a valid date/time."]}
+        )
+
+    def test_time_filtering_with_naive_datetime(self):
+        """In case a naive date is provided to the filter, it should be considered as UTC time"""
+        time_range_start = '2010-01-01T01:00:00Z'
+        time_range_end = '2010-01-02T01:00:00'
+        response = self.client.get(
+            '/api/datasets/'
+            f'?time_coverage_start__lte={time_range_end}'
+            f'&time_coverage_end__gte={time_range_start}'
+        )
+        self.assertJSONEqual(response.content, {
+            'next': None, 'previous': None, 'results': [self.DATASET_DICT_1, self.DATASET_DICT_2]
+        })
+
+    def test_zone_filtering(self):
+        """Test filtering datasets on geographic location using a WKT string.
+        If no SRID is specified, 4326 should be assumed.
+        """
+        # giving a location without SRID
+        response = self.client.get(
+            '/api/datasets/?geographic_location__geometry__intersects=POINT+%289+9%29')
+        self.assertJSONEqual(response.content, {
+            'next': None, 'previous': None, 'results': [self.DATASET_DICT_1]
+        })
+
+        # giving a location with SRID
+        response = self.client.get(
+            '/api/datasets/'
+            '?geographic_location__geometry__intersects=SRID%3D4326%3BPOINT+%289+9%29'
+        )
+        self.assertJSONEqual(response.content, {
+            'next': None, 'previous': None, 'results': [self.DATASET_DICT_1]
+        })
+
+    def test_source_instrument_filtering(self):
+        """Test filtering datasets on their instrument"""
+        response = self.client.get('/api/datasets/?source__instrument__short_name=HXT')
+        self.assertJSONEqual(response.content, {
+            'next': None, 'previous': None, 'results': [self.DATASET_DICT_1, self.DATASET_DICT_2]}
+        )
+
+    def test_source_platform_filtering(self):
+        """Test filtering datasets on a keyword which should be
+        contained in the platform short name
+        """
+        response = self.client.get('/api/datasets/?source__platform__short_name__contains=A340')
+        self.assertJSONEqual(response.content, {
+            'next': None, 'previous': None, 'results': [self.DATASET_DICT_2]
+        })
+
+    def test_source_filtering(self):
+        """Test filtering datasets on their platform and instrument"""
+        response = self.client.get(
+            '/api/datasets/'
+            '?source__platform__short_name=A340-600'
+            '&source__instrument__short_name=HXT')
+        self.assertJSONEqual(response.content, {
+            'next': None, 'previous': None, 'results': [self.DATASET_DICT_2]
+        })
+
+    def test_zone_and_time_filtering(self):
+        """Test filtering datasets on both time and location"""
+        date = '2010-01-01T07%3A00%3A00Z'
+        response = self.client.get(
+            '/api/datasets/'
+            f'?time_coverage_start__lte={date}&time_coverage_end__gte={date}'
+            '&geographic_location__geometry__contains=POINT+%289+9%29'
+        )
+        self.assertJSONEqual(response.content, {
+            'next': None, 'previous': None, 'results': [self.DATASET_DICT_1]}
+        )
+
+    def test_zone_source_and_time_filtering(self):
+        """Test filtering datasets based on time, source and zone simultaneously"""
+        date = '2010-01-01T07%3A00%3A00Z'
+        response = self.client.get(
+            '/api/datasets/'
+            f'?time_coverage_start__lte={date}&time_coverage_end__gte={date}'
+            '&geographic_location__geometry__contains=POINT+%289+9%29'
+            '&source__instrument__short_name__contains=HXT'
+        )
+        self.assertJSONEqual(response.content, {
+            'next': None, 'previous': None, 'results': [self.DATASET_DICT_1]}
+        )
+
+
+class DatasetURIFilterFilteringTests(django.test.TestCase):
+    """Tests dataset URIs filtering based on diverse parameters"""
+
+    fixtures = ["read_only_tests_data"]
+
+    def test_dataset_id_filtering(self):
+        """Test filtering dataset URIs on a dataset ID"""
+        response = self.client.get('/api/dataset_uris/?dataset=1')
+        self.assertJSONEqual(response.content, {
+            'next': None, 'previous': None, 'results': [{
+                "id": 1,
+                "name": "fileService",
+                "service": "local",
+                "uri": "file://localhost/some/test/file1.ext",
+                "dataset": 1
+            }]
+        })
