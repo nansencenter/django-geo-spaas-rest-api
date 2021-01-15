ebec1f67daab9291ac3b413599f091efbc92c15d
Adrien Perrin <adrien.perrin@nersc.no>
2021-01-15 10:36:03 +0000

    update setup.py version to 2.0.0rc1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="geospaas_rest_api",
-    version="1.1.2",
+    version="2.0.0rc1",
     author="Adrien Perrin",
     author_email="adrien.perrin@nersc.no",
     description="REST API for GeoSPaaS",
