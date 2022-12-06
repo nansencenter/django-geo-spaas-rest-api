e8a91ba1413552c302189b3b3d9d5aeb2d7c3637
Adrien Perrin <adrien.perrin@nersc.no>
2022-12-06 08:55:27 +0100

    remove v1

--- geospaas_rest_api/v1/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class GeospaasRestApiConfig(AppConfig):
-    name = 'geospaas_rest_api.v1'
--- geospaas_rest_api/v1/tests/urls.py
+++
@@ -1,8 +0,0 @@
-"""geospaas_rest_api testing URL Configuration"""
-
-from django.conf.urls import include, url
-
-urlpatterns = [
-    url(r'^', include('geospaas.urls')),
-    url(r'^api/', include('geospaas_rest_api.v1.urls')),
-]
