f6696da0e73caa8b5d7e4e89ae00e8d0b8f61bc3
Adrien Perrin <adrien.perrin@nersc.no>
2025-12-04 07:27:03 +0000

    update base API for new data model

--- geospaas_rest_api/base_api/filters.py
+++ geospaas_rest_api/base_api/filters.py
@@ -1,131 +1,46 @@
 """Custom filters for the base geospaas API"""
 import rest_framework_filters
-from django.contrib.gis.db.models import GeometryField
+from django.contrib.gis.db.models import GeometryField, JSONField
 from django_filters.rest_framework.filters import CharFilter
 
 import geospaas.catalog.models
 import geospaas.vocabularies.models
 
 
-class ScienceKeywordFilter(rest_framework_filters.FilterSet):
-    """Filter for ScienceKeyword"""
+class KeywordFilter(rest_framework_filters.FilterSet):
+    """Filter for Keywords"""
     class Meta:
-        model = geospaas.vocabularies.models.ScienceKeyword
+        model = geospaas.vocabularies.models.Keyword
         fields = {
-            'category': '__all__',
-            'topic': '__all__',
-            'term': '__all__',
-            'variable_level_1': '__all__',
-            'variable_level_2': '__all__',
-            'variable_level_3': '__all__',
-            'detailed_variable': '__all__',
+            'version': '__all__',
+            'kind': '__all__',
+            'data': '__all__',
         }
+        filter_overrides = {JSONField: {'filter_class': CharFilter}}
 
 
 class ParameterFilter(rest_framework_filters.FilterSet):
     """Filter for Parameters"""
     gcmd_science_keyword = rest_framework_filters.RelatedFilter(
-        ScienceKeywordFilter,
+        KeywordFilter,
         field_name='gcmd_science_keyword',
-        queryset=geospaas.vocabularies.models.ScienceKeyword.objects.all()
-    )
+        queryset=geospaas.vocabularies.models.Keyword.objects.all())
     class Meta:
         model = geospaas.vocabularies.models.Parameter
         fields = {
-            'standard_name': '__all__',
-            'short_name': '__all__',
-            'units': '__all__',
+            'version': '__all__',
+            'kind': '__all__',
+            'data': '__all__',
         }
+        filter_overrides = {JSONField: {'filter_class': CharFilter}}
 
 
-class InstrumentFilter(rest_framework_filters.FilterSet):
-    """Filter for Instruments"""
-    class Meta:
-        model = geospaas.vocabularies.models.Instrument
-        fields = {
-            'category': '__all__',
-            'instrument_class': '__all__',
-            'type': '__all__',
-            'subtype': '__all__',
-            'short_name': '__all__',
-            'long_name': '__all__',
-        }
-
-
-class PlatformFilter(rest_framework_filters.FilterSet):
-    """Filter for Platforms"""
-    class Meta:
-        model = geospaas.vocabularies.models.Platform
-        fields = {
-            'category': '__all__',
-            'series_entity': '__all__',
-            'short_name': '__all__',
-            'long_name': '__all__',
-        }
-
-
-class DataCenterFilter(rest_framework_filters.FilterSet):
-    """Filter for DataCenters"""
-    class Meta:
-        model = geospaas.vocabularies.models.DataCenter
-        fields = {
-            'bucket_level0': '__all__',
-            'bucket_level1': '__all__',
-            'bucket_level2': '__all__',
-            'bucket_level3': '__all__',
-            'short_name': '__all__',
-            'long_name': '__all__',
-            'data_center_url': '__all__',
-        }
-
-
-class ISOTopicCategoryFilter(rest_framework_filters.FilterSet):
-    """Filter for ISOTopicCategories"""
-    class Meta:
-        model = geospaas.vocabularies.models.ISOTopicCategory
-        fields = {'name': '__all__'}
-
-
-class LocationFilter(rest_framework_filters.FilterSet):
-    """Filter for Locations"""
-    class Meta:
-        model = geospaas.vocabularies.models.Location
-        fields = {
-            'category': '__all__',
-            'type': '__all__',
-            'subregion1': '__all__',
-            'subregion2': '__all__',
-            'subregion3': '__all__',
-        }
-
-
-class GeographicLocationFilter(rest_framework_filters.FilterSet):
-    """Filter for GeographicLocations"""
+class TagFilter(rest_framework_filters.FilterSet):
+    """Filter for Tags"""
 
     class Meta:
-        model = geospaas.catalog.models.GeographicLocation
-        fields = {'geometry': '__all__'}
-        # We have to define the filter used for GeometryFields
-        # because it's not supported natively.
-        filter_overrides = {
-             GeometryField: {
-                 'filter_class': CharFilter
-             }
-        }
-
-
-class SourceFilter(rest_framework_filters.FilterSet):
-    """Filter for Sources"""
-    instrument = rest_framework_filters.RelatedFilter(
-        InstrumentFilter,
-        field_name='instrument',
-        queryset=geospaas.vocabularies.models.Instrument.objects.all()
-    )
-    platform = rest_framework_filters.RelatedFilter(
-        PlatformFilter,
-        field_name='platform',
-        queryset=geospaas.vocabularies.models.Platform.objects.all()
-    )
+        model = geospaas.catalog.models.Tag
+        fields = {'name': '__all__', 'value': '__all__'}
 
 
 class PersonnelFilter(rest_framework_filters.FilterSet):
@@ -157,36 +72,21 @@ class RoleFilter(rest_framework_filters.FilterSet):
 
 class DatasetFilter(rest_framework_filters.FilterSet):
     """Filter for Datasets"""
-    iso_topic_category = rest_framework_filters.RelatedFilter(
-        ISOTopicCategoryFilter,
-        field_name='ISO_topic_category',
-        queryset=geospaas.vocabularies.models.ISOTopicCategory.objects.all()
-    )
-    data_center = rest_framework_filters.RelatedFilter(
-        DataCenterFilter,
-        field_name='data_center',
-        queryset=geospaas.vocabularies.models.DataCenter.objects.all()
-    )
-    source = rest_framework_filters.RelatedFilter(
-        SourceFilter,
-        field_name='source',
-        queryset=geospaas.catalog.models.Source.objects.all()
-    )
-    geographic_location = rest_framework_filters.RelatedFilter(
-        GeographicLocationFilter,
-        field_name='geographic_location',
-        queryset=geospaas.catalog.models.GeographicLocation.objects.all()
-    )
-    gcmd_location = rest_framework_filters.RelatedFilter(
-        LocationFilter,
-        field_name='gcmd_location',
-        queryset=geospaas.vocabularies.models.Location.objects.all()
-    )
+    tags = rest_framework_filters.RelatedFilter(
+        TagFilter,
+        field_name='tags',
+        queryset=geospaas.catalog.models.Tag.objects.all(),
+        distinct=True)
+    keywords = rest_framework_filters.RelatedFilter(
+        KeywordFilter,
+        field_name='keywords',
+        queryset=geospaas.vocabularies.models.Keyword.objects.all(),
+        distinct=True)
     parameters = rest_framework_filters.RelatedFilter(
         ParameterFilter,
         field_name='parameters',
-        queryset=geospaas.vocabularies.models.Parameter.objects.all()
-    )
+        queryset=geospaas.vocabularies.models.Parameter.objects.all(),
+        distinct=True)
 
     class Meta:
         model = geospaas.catalog.models.Dataset
@@ -197,8 +97,10 @@ class DatasetFilter(rest_framework_filters.FilterSet):
             'summary': '__all__',
             'time_coverage_start': '__all__',
             'time_coverage_end': '__all__',
-            'access_constraints': '__all__'
+            'access_constraints': '__all__',
+            'location': '__all__',
         }
+        filter_overrides = {GeometryField: {'filter_class': CharFilter}}
 
 
 class DatasetURIFilter(rest_framework_filters.FilterSet):
@@ -210,11 +112,7 @@ class DatasetURIFilter(rest_framework_filters.FilterSet):
     )
     class Meta:
         model = geospaas.catalog.models.DatasetURI
-        fields = {
-            'name': '__all__',
-            'service': '__all__',
-            'uri': '__all__',
-        }
+        fields = {'uri': '__all__'}
 
 
 class DatasetRelationshipFilter(rest_framework_filters.FilterSet):
@@ -229,5 +127,3 @@ class DatasetRelationshipFilter(rest_framework_filters.FilterSet):
         field_name='parent',
         queryset=geospaas.catalog.models.Dataset.objects.all()
     )
-
-
--- geospaas_rest_api/base_api/views.py
+++ geospaas_rest_api/base_api/views.py
@@ -7,32 +7,25 @@ import geospaas_rest_api.base_api.filters as filters
 import geospaas_rest_api.base_api.serializers as serializers
 
 
-class GeographicLocationViewSet(ReadOnlyModelViewSet):
-    """API endpoint to view GeographicLocations"""
-    queryset = geospaas.catalog.models.GeographicLocation.objects.all()
-    serializer_class = serializers.GeographicLocationSerializer
-    filterset_class = filters.GeographicLocationFilter
+class KeywordViewSet(ReadOnlyModelViewSet):
+    """API endpoint to view Keywords"""
+    queryset = geospaas.vocabularies.models.Keyword.objects.all()
+    serializer_class = serializers.KeywordSerializer
+    filterset_class = filters.KeywordFilter
 
 
-class SourceViewSet(ReadOnlyModelViewSet):
-    """API endpoint to view Sources"""
-    queryset = geospaas.catalog.models.Source.objects.all()
-    serializer_class = serializers.SourceSerializer
-    filterset_class = filters.SourceFilter
+class ParameterViewSet(ReadOnlyModelViewSet):
+    """API endpoint to view Parameters"""
+    queryset = geospaas.vocabularies.models.Parameter.objects.all()
+    serializer_class = serializers.ParameterSerializer
+    filterset_class = filters.ParameterFilter
 
 
-class InstrumentViewSet(ReadOnlyModelViewSet):
-    """API endpoint to view Instruments"""
-    queryset = geospaas.vocabularies.models.Instrument.objects.all()
-    serializer_class = serializers.InstrumentSerializer
-    filterset_class = filters.InstrumentFilter
-
-
-class PlatformViewSet(ReadOnlyModelViewSet):
-    """API endpoint to view Platforms"""
-    queryset = geospaas.vocabularies.models.Platform.objects.all()
-    serializer_class = serializers.PlatformSerializer
-    filterset_class = filters.PlatformFilter
+class TagViewSet(ReadOnlyModelViewSet):
+    """API endpoint to view Tag"""
+    queryset = geospaas.catalog.models.Tag.objects.all()
+    serializer_class = serializers.TagSerializer
+    filterset_class = filters.TagFilter
 
 
 class PersonnelViewSet(ReadOnlyModelViewSet):
@@ -56,13 +49,6 @@ class DatasetViewSet(ReadOnlyModelViewSet):
     filterset_class = filters.DatasetFilter
 
 
-class ParameterViewSet(ReadOnlyModelViewSet):
-    """API endpoint to view Parameters"""
-    queryset = geospaas.vocabularies.models.Parameter.objects.all()
-    serializer_class = serializers.ParameterSerializer
-    filterset_class = filters.ParameterFilter
-
-
 class DatasetURIViewSet(ReadOnlyModelViewSet):
     """API endpoint to view DatasetURIs"""
     queryset = geospaas.catalog.models.DatasetURI.objects.all()
@@ -75,31 +61,3 @@ class DatasetRelationshipViewSet(ReadOnlyModelViewSet):
     queryset = geospaas.catalog.models.DatasetRelationship.objects.all()
     serializer_class = serializers.DatasetRelationshipSerializer
     filterset_class = filters.DatasetRelationshipFilter
-
-
-class DataCenterViewSet(ReadOnlyModelViewSet):
-    """API endpoint to view DataCenters"""
-    queryset = geospaas.vocabularies.models.DataCenter.objects.all()
-    serializer_class = serializers.DataCenterSerializer
-    filterset_class = filters.DataCenterFilter
-
-
-class ISOTopicCategoryViewSet(ReadOnlyModelViewSet):
-    """API endpoint to view ISOTopicCategories"""
-    queryset = geospaas.vocabularies.models.ISOTopicCategory.objects.all()
-    serializer_class = serializers.ISOTopicCategorySerializer
-    filterset_class = filters.ISOTopicCategoryFilter
-
-
-class ScienceKeywordViewSet(ReadOnlyModelViewSet):
-    """API endpoint to view ScienceKeywords"""
-    queryset = geospaas.vocabularies.models.ScienceKeyword.objects.all()
-    serializer_class = serializers.ScienceKeywordSerializer
-    filterset_class = filters.ScienceKeywordFilter
-
-
-class LocationViewSet(ReadOnlyModelViewSet):
-    """API endpoint to view Locations"""
-    queryset = geospaas.vocabularies.models.Location.objects.all()
-    serializer_class = serializers.LocationSerializer
-    filterset_class = filters.LocationFilter
